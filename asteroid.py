30a7a0a01faf640322d01014fe7d22c748c7bc44
Taink <tainkk@protonmail.com>
2025-01-19 06:52:41 +0100

    Split asteroids on bullet collisions

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.surface import Surface
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float):
@@ -11,3 +13,14 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS: # small asteroids
+            return
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        split_angle = random.uniform(20, 50)
+        new_asteroid_1.velocity = self.velocity.rotate(split_angle) * 1.2
+        new_asteroid_2.velocity = self.velocity.rotate(-split_angle)
--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if shot.collides_with(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill((0, 0, 0))
