43501cac1334d1d7c4aec3495c9d4d6c1cf4703d
Taink <tainkk@protonmail.com>
2025-01-19 06:40:31 +0100

    Limit fire rate

--- player.py
+++ player.py
@@ -1,14 +1,14 @@
 from pygame.surface import Surface
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_PROJECTILE_SPEED, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_PROJECTILE_SPEED, PLAYER_SHOOT_COOLDOWN, PLAYER_TURN_SPEED, PLAYER_MOVE_SPEED
 import pygame
-from constants import PLAYER_MOVE_SPEED
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x: float, y: float):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -29,11 +29,16 @@ class Player(CircleShape):
         self.position += forward * PLAYER_MOVE_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_PROJECTILE_SPEED
 
     def update(self, dt: float):
         keys = pygame.key.get_pressed()
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
